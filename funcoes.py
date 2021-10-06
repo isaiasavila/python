62c6ff57e9257795da1e4fbd77c201da941c3a72
isaiasavila <84739990+isaiasavila@users.noreply.github.com>
2021-10-06 08:59:23 -0300

    Add files via upload

--- funcoes.py
+++ funcoes.py
@@ -99,7 +99,114 @@ def MusicMemoria(n):
     import os
     from winsound import Beep
     t = '.'
-    for i in range (n):
+    for i in range(n):
         t = t + '.'
         Beep(400, 400)
     os.system('cls')
+
+def calcular_valor(valor_prod, qtde, moeda="real", desconto=None, acrescimo=None):
+    v_bruto = valor_prod * qtde
+    # 06/10/21
+    dolar = 5.48
+    euro = 6.32
+
+    if desconto:
+        v_liquido = v_bruto - (v_bruto * (desconto / 100))
+    elif acrescimo:
+        v_liquido = v_bruto + (v_bruto * (acrescimo / 100))
+    else:
+        v_liquido = v_bruto
+   
+    if moeda == 'real':
+        return v_liquido
+    elif moeda == 'dolar':
+        return v_liquido * dolar
+    elif moeda == 'euro':
+        return v_liquido * euro
+    else:
+        print("Moeda não cadastrada!")
+        return 0
+
+   
+valor_a_pagar = calcular_valor(valor_prod=32, qtde=5, desconto=5)
+print(f"O valor final da conta é {valor_a_pagar}")
+
+def calcular_valor_kw(valor_prod, qtde, moeda="real", **kwargs):
+    v_bruto = valor_prod * qtde
+   
+    if 'desconto' in kwargs:
+        desconto = kwargs['desconto']
+        v_liquido = v_bruto - (v_bruto * (desconto / 100))
+    elif 'acrescimo' in kwargs:
+        acrescimo = kwargs['acrescimo']
+        v_liquido = v_bruto + (v_bruto * (acrescimo / 100))
+    else:
+        v_liquido = v_bruto
+   
+    if moeda == 'real':
+        return v_liquido
+    elif moeda == 'dolar':
+        return v_liquido * 5
+    elif moeda == 'euro':
+        return v_liquido * 5.7
+    else:
+        print("Moeda não cadastrada!")
+        return 0
+
+   
+valor_a_pagar = calcular_valor_kw(valor_prod=32, qtde=5, desconto=5)
+print(f"O valor final da conta é {valor_a_pagar}")
+
+def converter_mes_para_extenso(data):
+    mes = '''janeiro fevereiro março
+      abril maio junho julho agosto
+      setembro outubro novembro
+      dezembro'''.split()
+    d, m, a = data.split('/')
+    mes_extenso = mes[int(m) - 1] # O mês 1, estará na posição 0!
+    return f'{d} de {mes_extenso} de {a}'
+
+print(converter_mes_para_extenso('10/05/2021'))
+
+def calcular_desconto(valor, desconto=0): 
+    # O parâmetro desconto possui zero valor default
+    valor_com_desconto = valor - (valor * desconto)
+    return valor_com_desconto
+
+def imprimir_parametros(*args):
+    qtde_parametros = len(args)
+    print(f"Quantidade de parâmetros = {qtde_parametros}")
+   
+    for i, valor in enumerate(args):
+        print(f"Posição = {i}, valor = {valor}")
+
+       
+print("\nChamada 1")        
+imprimir_parametros("São Paulo", 10, 23.78, "João")
+
+print("\nChamada 2")        
+imprimir_parametros(10, "São Paulo")
+
+def imprimir_parametros(**kwargs):
+    print(f"Tipo de objeto recebido = {type(kwargs)}\n")
+    qtde_parametros = len(kwargs)
+    print(f"Quantidade de parâmetros = {qtde_parametros}")
+   
+    for chave, valor in kwargs.items():
+        print(f"variável = {chave}, valor = {valor}")
+
+       
+print("\nChamada 1")        
+imprimir_parametros(cidade="São Paulo", idade=33, nome="João")
+
+print("\nChamada 2")        
+imprimir_parametros(desconto=10, valor=100)
+
+# Exemplo simples de lambda
+somar = lambda x, y: x + y
+somar(x=5, y=3)
+
+# Exemplo de enumerate
+nome = "Guido"
+for i, c in enumerate(nome):
+    print(f"Posição = {i}, valor = {c}")
