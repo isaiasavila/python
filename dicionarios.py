dd98f114415b823b8cdbda6ff08b99c816c19f0a
isaiasavila <84739990+isaiasavila@users.noreply.github.com>
2021-10-07 17:19:06 -0300

    Update dicionarios.py

--- dicionarios.py
+++ dicionarios.py
@@ -1,4 +1,35 @@
 #Dicionários
+def funcoes_basicas():
+    dici_1 = {}
+    dici_1['nome'] = "João"
+    dici_1['idade'] = 30
+    # Exemplo 2 - Criação de dicionário usando um par elementos na forma, chave : valor
+    dici_2 = {'nome': 'João', 'idade': 30}
+    # Exemplo 3 - Criação de dicionário com uma lista de tuplas. Cada tupla representa um par chave : valor
+    dici_3 = dict([('nome', "João"), ('idade', 30)])
+    # Exemplo 4 - Criação de dicionário com a função built-in zip() e duas listas, uma com as chaves, outra com os valores.
+    dici_4 = dict(zip(['nome', 'idade'], ["João", 30]))
+    # Testando se as diferentes construções resultam em objetos iguais.
+    print(dici_1 == dici_2 == dici_3 == dici_4)
+    # Exemplo 1 - Criação de dicionário vazio, com atribuição posterior de chave e valor 
+    cadastro = {
+                'nome' : ['João', 'Ana', 'Pedro', 'Marcela'],
+                'cidade' : ['São Paulo', 'São Paulo', 'Rio de Janeiro', 'Curitiba'],
+                'idade' : [25, 33, 37, 18]
+                }
+    print("len(cadastro) = ", len(cadastro))
+    print("\n cadastro.keys() = \n", cadastro.keys())
+    print("\n cadastro.values() = \n", cadastro.values())
+    print("\n cadastro.items() = \n", cadastro.items())
+    print("\n cadastro['nome'] = ", cadastro['nome'])
+    print("\n cadastro['nome'][2] = ", cadastro['nome'][2])
+    print("\n cadastro['idade'][2:] = ", cadastro['idade'][2:])
+    print(len(cadastro['nome']))
+    print(len(cadastro['cidade']))
+    print(len(cadastro['idade']))
+    qtde_itens = sum([len(cadastro[chave]) for chave in cadastro])
+    print(f"\n\nQuantidade de elementos no dicionário = {qtde_itens}")
+    
 def exemplo_um():
     aurelio={'denomiação':'ilha solteira','população':23000,'renda':1500}
     print(aurelio)
