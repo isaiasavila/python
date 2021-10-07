e9042e388f73454c713924dc22bb26e1da252d33
isaiasavila <84739990+isaiasavila@users.noreply.github.com>
2021-10-07 12:27:49 -0300

    Update textos.py

--- textos.py
+++ textos.py
@@ -31,8 +31,59 @@ def clean_strings(strings):
     return result
 
 def remove_punctuation(value):
-    return re.sub('[!#?.,;]', '', value)    
-
+        return re.sub('[!#?.,;]', '', value)
+    
+def exemplo_operacoes_texto():
+    texto = "Aprendendo Python na disciplina de linguagem de programação."
+    # Tamanho da variável texto
+    print(f"Tamanho do texto = {len(texto)}")
+    # A 'palavra' está no texto?
+    print(f"Python in texto = {'Python' in texto}")
+    # Quantidade do valor de parâmetro na variável 
+    print(f"Quantidade de y no texto = {texto.count('y')}")
+    # Retorna os seis primeiros caracteres da variável
+    print(f"As 5 primeiras letras são: {texto[0:5]}")
+    # Converte o texto para maiúsculo
+    print(texto.upper())
+    # Converte o texto para minúsculo
+    print(texto.lower())
+    # Substitui a letra i por XX
+    print(texto.replace("ça", 'ca'))
+    # fatia a string, como não foi indicado o separador, por padrão usa o espaço
+    palavras = texto.split()
+    # imprime a string fatiada
+    print(f"palavras = {palavras}")
+    # imprime a quantidade das palavras
+    print(f"Tamanho de palavras = {len(palavras)}")
+    ############### PLN no braço
+    # Transforma tudo para letras minúsculas
+    texto = texto.lower()
+    # Retira do texto tudo que não for palavra
+    texto = texto.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace("\n", " ")
+    # Cria um lista com as palavras
+    lista_palavras = texto.split()
+    # Guarda a quantidade de incidência das palavras 'string' e 'strings'
+    total = lista_palavras.count("string") + lista_palavras.count("strings")
+    # Mostra a mensagem do total
+    print(f"Quantidade de vezes que string ou strings aparecem = {total}")
+    vogais = ('a', 'e', 'i', 'o', 'u')
+    for vogal in vogais:
+        # Imprime o índice, seguido do valor da lista vogais
+        print (f'Posição = {vogais.index(vogal)}, valor = {vogal}')
+    # Outra forma de fazer
+    vogais = []
+    # tipo do objeto
+    print(f"Tipo do objeto vogais = {type(vogais)}")
+    # método para adicionar elemento na lista vogais
+    vogais.append('a')
+    vogais.append('e')
+    vogais.append('i')
+    vogais.append('o')
+    vogais.append('u')
+    for p, x in enumerate(vogais):
+        # Imprime o índice, seguido do valor da lista vogais
+        print(f"Posição = {p}, valor = {x}")
+    
 def mostrar(string=None):
     if string is not None:
         print(string)
